b78f2e2e884ddf1636239d215df044100620f6a8
Kyle Qian <cqian@stanford.edu>
2016-05-01 23:53:36 -0700

    Added pause on content hover

--- nature.js
+++ nature.js
@@ -1,27 +1,38 @@
 $(function(){
-  console.log("We are loaded!");
+	console.log("We are loaded!");
 
-  var posts = $(".posts ul")
+	var posts = $(".posts ul")
 
-  // Slick main
-  posts.slick({
-  	autoplay: true,
-  	autoplaySpeed: 1000,
-  	dots: true,
-  	draggable: false,
-  	mobileFirst: true,
-  	fade: true,
-  	speed: 1500,
-  	pauseOnDotsHover: true,
-  });
+	// Slick main
+	posts.slick({
+		autoplay: true,
+		autoplaySpeed: 1000,
+		dots: true,
+		draggable: false,
+		mobileFirst: true,
+		fade: true,
+		speed: 1500,
+		pauseOnDotsHover: true,
+		pauseOnHover: false
+	});
 
-  // Pauses carousel upon hovering on arrows
-  $(".slick-arrow").hover(
-  	function() {
-  		posts.slick("slickPause");
-  	},
-  	function() {
-  		posts.slick("slickPlay");
-  	}
-  );
+	// Pauses carousel upon hovering on arrows
+	$(".slick-arrow").hover(
+		function() {
+			posts.slick("slickPause");
+		},
+		function() {
+			posts.slick("slickPlay");
+		}
+	);
+
+	// Pauses carousel upon hovering on content
+	$(".post-content").hover(
+		function() {
+			posts.slick("slickPause");
+		},
+		function() {
+			posts.slick("slickPlay");
+		}
+	);
 });
