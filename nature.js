cecced8ce958174e580edd3359d4067e840876f4
Kyle Qian <cqian@stanford.edu>
2016-05-01 23:03:45 -0700

    Added Slick carousel

--- nature.js
+++ nature.js
@@ -1,3 +1,12 @@
 $(function(){
   console.log("We are loaded!");
+
+  $(".posts ul").slick({
+  	adaptiveHeight: true,
+  	autoplay: true,
+  	autoplaySpeed: 1000,
+  	dots: true,
+  	draggable: false,
+  	mobileFirst: true
+  });
 });
