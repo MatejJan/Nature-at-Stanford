c1bdb23472504c9746663220cf25f204efa50cf8
Kyle Qian <cqian@stanford.edu>
2016-06-08 02:51:46 -0700

    added back modals

--- static/js/home.js
+++ static/js/home.js
@@ -1,66 +1,63 @@
 $(function(){
 
-	$('.slick-content-wrapper').slick();
+	// // Get parent element of all the posts.
+	// var $posts = $(".posts");
+
+	// // Whether autoplay should be off
+	// var stopAutoplay = false;
+
+	// // Slick main
+	// $posts.slick({
+	// 	autoplay: true,
+	// 	autoplaySpeed: 10000,
+	// 	draggable: false,
+	// 	mobileFirst: true,
+	// 	fade: true,
+	// 	speed: 500,
+	// 	pauseOnHover: false,
+	// 	arrows: false
+	// });
+
+	// var getVideoClass = function($element) {
+	// 	cssClasses = $element.attr('class').split(' ');
+	// 	for (var i=0; i<cssClasses.length;i++) {
+	// 		if (cssClasses[i].indexOf('video') > -1) {
+	// 			return cssClasses[i]
+	// 		}
+	// 	}
+	// 	return null;
+	// };
 
 
-	// Get parent element of all the posts.
-	var $posts = $(".posts");
+	// var slideIndex = 0;
+	// var videoClass = getVideoClass($('.posts .post').eq(slideIndex));
 
-	// Whether autoplay should be off
-	var stopAutoplay = false;
+	// var switchVideo = function(newVideoClass){
+	// 	// Put the new video underneath the current one.
+	// 	$('.background-video .' + newVideoClass).css('z-index', '1');
 
-	// Slick main
-	$posts.slick({
-		autoplay: true,
-		autoplaySpeed: 10000,
-		draggable: false,
-		mobileFirst: true,
-		fade: true,
-		speed: 500,
-		pauseOnHover: false,
-		arrows: false
-	});
+	// 	// Fade the current video to 0.
+	// 	$('.background-video .' + videoClass).css('opacity', '0');
 
-	var getVideoClass = function($element) {
-		cssClasses = $element.attr('class').split(' ');
-		for (var i=0; i<cssClasses.length;i++) {
-			if (cssClasses[i].indexOf('video') > -1) {
-				return cssClasses[i]
-			}
-		}
-		return null;
-	};
+	// 	var oldVideoClass = videoClass
 
+	// 	setTimeout(function(){
+	// 		$('.background-video .' + oldVideoClass).css('z-index', '0').css('opacity', '1');
+	// 		$('.background-video .' + newVideoClass).css('z-index', '2');
+	// 	}, 2100);
 
-	var slideIndex = 0;
-	var videoClass = getVideoClass($('.posts .post').eq(slideIndex));
+	// 	videoClass = newVideoClass;
+	// };
 
-	var switchVideo = function(newVideoClass){
-		// Put the new video underneath the current one.
-		$('.background-video .' + newVideoClass).css('z-index', '1');
+	// $('.background-video .' + videoClass).css('opacity', '1').css('zIndex', '2');
+	// setTimeout(function(){
+	// 	$('.background-video video').css('opacity', '1');
+	// }, 2100);
 
-		// Fade the current video to 0.
-		$('.background-video .' + videoClass).css('opacity', '0');
-
-		var oldVideoClass = videoClass
-
-		setTimeout(function(){
-			$('.background-video .' + oldVideoClass).css('z-index', '0').css('opacity', '1');
-			$('.background-video .' + newVideoClass).css('z-index', '2');
-		}, 2100);
-
-		videoClass = newVideoClass;
-	};
-
-	$('.background-video .' + videoClass).css('opacity', '1').css('zIndex', '2');
-	setTimeout(function(){
-		$('.background-video video').css('opacity', '1');
-	}, 2100);
-
-	$posts.on('afterChange', function(event, slick, currentSlide){
-		var newVideoClass = getVideoClass($('.posts .post').eq(currentSlide));
-		switchVideo(newVideoClass);
-	});
+	// $posts.on('afterChange', function(event, slick, currentSlide){
+	// 	var newVideoClass = getVideoClass($('.posts .post').eq(currentSlide));
+	// 	switchVideo(newVideoClass);
+	// });
 
 	$('.portfolio-box').magnificPopup({
 	  type: 'ajax',
