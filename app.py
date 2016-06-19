653786af29cd285527b7d4d6001edc93759f6230
Kyle Qian <cqian@stanford.edu>
2016-06-18 17:11:49 -0700

    changed boxes to serve static pages without Flask router

--- app.py
+++ app.py
@@ -5,9 +5,5 @@ app = Flask(__name__, static_url_path='')
 def home():
 	return render_template('home.html')
 
-@app.route('/projects/<int:project_id>')
-def project_card(project_id):
-	return app.send_static_file('projects/project-card-%d.html' % project_id)
-
 if __name__ == '__main__':
 	app.run(debug=True)
