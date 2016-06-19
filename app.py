9211b85c8dc03a83d717983af79ba5bdeae8e6ed
Kyle Qian <cqian@stanford.edu>
2016-06-18 18:04:13 -0700

    Boxes now show pictures in cgi-bin

--- app.py
+++ app.py
@@ -5,5 +5,10 @@ app = Flask(__name__, static_url_path='')
 def home():
 	return render_template('home.html')
 
+@app.route('/app.cgi/projects/<int:project_id>')
+@app.route('/projects/<int:project_id>')
+def project_card(project_id):
+	return render_template('projects/project-card-%d.html' % project_id)
+
 if __name__ == '__main__':
 	app.run(debug=True)
